57bbbb7adde103c4199e974f090eb5d341ac1a66
Caio Filipini <caiofilipini@gmail.com>
2010-09-29 13:22:15 -0300

    added some javadocs the the converter

--- src/main/java/org/pojongo/core/conversion/DocumentToObjectConverter.java
+++ src/main/java/org/pojongo/core/conversion/DocumentToObjectConverter.java
@@ -7,15 +7,31 @@ import net.vidageek.mirror.dsl.Mirror;
 
 import com.mongodb.DBObject;
 
+/**
+ * Class responsible for converting <code>DBObject</code> instances<br>
+ * to a given Java Object based on the matching fields.
+ * 
+ * @author Caio Filipini
+ */
 public class DocumentToObjectConverter {
 
 	private final Mirror mirror;
 	private DBObject document;
 	
+	/**
+	 * Default constructor.
+	 */
 	public DocumentToObjectConverter() {
 		this.mirror = new Mirror();
 	}
 	
+	/**
+	 * Builder method used to configure which <code>DBObject</code> should be converted.
+	 * 
+	 * @param document the <code>DBObject</code> to be converted to Java object.
+	 * @return the converter.
+	 * @throws IllegalArgumentException if <code>document</code> is null.
+	 */
 	public DocumentToObjectConverter from(final DBObject document) {
 		if (document == null) {
 			throw new IllegalArgumentException("cannot convert a null document");
@@ -24,6 +40,18 @@ public class DocumentToObjectConverter {
 		return this;
 	}
 	
+	/**
+	 * Converts the previously configured <code>DBObject</code> to a corresponding instance<br />
+	 * of the specified Java class <code>objectType</code>.<br /><br/>
+	 * 
+	 * The conversion is done by reflecting <code>objectType</code>'s attributes and finding<br />
+	 * corresponding fields in <code>DBObject<code>. If a matching field is found, its value is<br />
+	 * set on the target object, preserving the data type returned by MongoDB's driver.
+	 * 
+	 * @param <T> the generic type for objectType.
+	 * @param objectType the type to be converted to.
+	 * @return an instance of <code>objectType</code> populated with corresponding values from MongoDB's document.
+	 */
 	public <T extends Object> T to(final Class<T> objectType) {
 		T instance = instanceFor(objectType);
 		List<Field> fields = getFieldsFor(objectType);
