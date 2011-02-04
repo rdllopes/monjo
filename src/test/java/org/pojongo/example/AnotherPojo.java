528b07b589d9c5c993067a4e17485a7ec7ad71da
rdlopes <rdlopes@sp.r7.com>
2011-02-04 20:56:39 -0200

    Correção do pojongo para objetos internos

---
+++ src/test/java/org/pojongo/example/AnotherPojo.java
@@ -0,0 +1,28 @@
+package org.pojongo.example;
+
+
+public class AnotherPojo extends AbstractObject {
+		
+		private User user;
+
+		public String getDescription() {
+			return description;
+		}
+
+		public void setDescription(String description) {
+			this.description = description;
+		}
+
+		private String description;
+
+		public User getUser() {
+			return user;
+		}
+
+		public void setUser(User user) {
+			this.user = user;
+		}
+
+
+		
+}
---
+++ src/test/java/org/pojongo/example/User.java
@@ -0,0 +1,15 @@
+package org.pojongo.example;
+
+public class User {
+	
+	private String name;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+}
