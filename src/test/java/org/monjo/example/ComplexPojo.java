57fe11ce1f2b31802be91b2490135b968e04fdd1
rdlopes <rdlopes@sp.r7.com>
2011-03-29 16:20:40 -0300

    adiciona interger num complex pojo para ser mais fácil testar

--- src/test/java/org/monjo/example/ComplexPojo.java
+++ src/test/java/org/monjo/example/ComplexPojo.java
@@ -7,6 +7,16 @@ import org.monjo.core.annotations.Reference;
 public class ComplexPojo extends AbstractObject {
 	
 	private Category category;
+	
+	public Integer getTest() {
+		return test;
+	}
+
+	public void setTest(Integer test) {
+		this.test = test;
+	}
+
+	private Integer test;
 
 	public String getDescription() {
 		return description;
